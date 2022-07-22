cf68c9571be2e2c2bddb8391c377704f7c24b84d
loi777 <35152365+loi777@users.noreply.github.com>
2022-07-22 06:13:34 -0300

    Update main.py
    mantendo em dia

--- main.py
+++ main.py
@@ -22,17 +22,15 @@ import os
 from sklearn.metrics import accuracy_score, precision_score, classification_report, auc, roc_curve, plot_roc_curve, roc_auc_score, confusion_matrix, precision_recall_curve, f1_score
 from matplotlib import pyplot as plt
 
-#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-#subset_name = '_j-smol/'
-#base_dir = './data/'
-#input_dir = '1-roi/'
-#seg_subdir = '2-seg/'
-#wave_subdir = '3-wave/'
-#seg_model_file = "./models/x_grey_3000.pth"
+bug_test = './data/Jersey_SMix/J357/J357_S1_1.png'
 
-testeImg = './data/Jersey_SMix/J71/J71_S2_0.png'
-testeImg2 = './data/Jersey_SMix/J71/J71_S1_0.png'
+testeImg = './data/Jersey_SMix/J71/J71_S1_0.png'
+testeImg2 = './data/Jersey_SMix/J71/J71_S2_0.png'
+
+
+
+
 
 # test gen_graph
 def amain():
@@ -53,8 +51,41 @@ def amain():
 
     return
 
-# test and save both match types individually
+
+
+# testando RANSAC output
 def main():
+    MatchTeste = gen_graph.graph_routine(testeImg)
+    MatchTeste2 = gen_graph.graph_routine(testeImg2)
+    ResultadoMatch = bovine_matcher.match_recursive(MatchTeste, MatchTeste2)
+    
+    
+    file = open('results/RANSACconsole.txt', 'w')
+    
+    for match in ResultadoMatch :
+        file.write('[%3d : %3d]  [%3d : %3d]\n' % (match[0][1] , match[0][0], match[1][1], match[1][0]))
+    file.write('-----==AGORA O RANSAC==-----\n\n')
+    
+    number_of_ransac_confirmations = 0
+    ResultadoRansac = bovine_matcher.ransac_filter(ResultadoMatch)
+    
+    for match in range(len(ResultadoRansac[0])) :
+        if (ResultadoRansac[0][match]) :
+            number_of_ransac_confirmations += 1
+        file.write('Ransac: %d  ||  ' % ResultadoRansac[0][match])
+        file.write('[%3d : %3d]  [%3d : %3d]\n' % (ResultadoRansac[1][match][1], ResultadoRansac[1][match][0], ResultadoRansac[2][match][1], ResultadoRansac[2][match][0]))
+    file.write('soma do ransac: %d -----==END==-----\n\n\n' % number_of_ransac_confirmations)
+    
+    file.close()
+    
+    bovine_matcher.luiz_draw(testeImg, testeImg2, ResultadoRansac)
+    
+    return
+
+
+
+# test and save both match types individually
+def lelelmain():
     MatchTeste = gen_graph.graph_routine(testeImg)
     MatchTeste2 = gen_graph.graph_routine(testeImg2)
     
@@ -65,7 +96,7 @@ def main():
     file = open('results/testeMatcher.txt', 'w')
     
     for match in ResultadoMatchRecursivo :
-        file.write('[%3d : %3d]  [%3d : %3d]\n' % (match[0][0] , match[0][1], match[1][0], match[1][1]))
+        file.write('[%3d : %3d]  [%3d : %3d]\n' % (match[0][1] , match[0][0], match[1][1], match[1][0]))
     file.write('-----==AGORA O RANSAC==-----\n\n')
     
     number_of_ransac_confirmations = 0
@@ -75,13 +106,13 @@ def main():
         if (ResultadoRansac[0][match]) :
             number_of_ransac_confirmations += 1
         file.write('Ransac: %d  ||  ' % ResultadoRansac[0][match])
-        file.write('[%3d : %3d]  [%3d : %3d]\n' % (ResultadoRansac[1][match][0], ResultadoRansac[1][match][1], ResultadoRansac[2][match][0], ResultadoRansac[2][match][1]))
+        file.write('[%3d : %3d]  [%3d : %3d]\n' % (ResultadoRansac[1][match][1], ResultadoRansac[1][match][0], ResultadoRansac[2][match][1], ResultadoRansac[2][match][0]))
     file.write('soma do ransac: %d -----==END DO RECURSIVO==-----\n\n\n' % number_of_ransac_confirmations)
     
     
     
     for match in ResultadoMatch :
-        file.write('[%3d : %3d]  [%3d : %3d]\n' % (match[0][0] , match[0][1], match[1][0], match[1][1]))
+        file.write('[%3d : %3d]  [%3d : %3d]\n' % (match[0][1] , match[0][0], match[1][1], match[1][0]))
     file.write('-----==AGORA O RANSAC==-----\n\n')
     
     number_of_ransac_confirmations = 0
@@ -91,15 +122,17 @@ def main():
         if (ResultadoRansac[0][match]) :
             number_of_ransac_confirmations += 1
         file.write('Ransac: %d  ||  ' % ResultadoRansac[0][match])
-        file.write('[%3d : %3d]  [%3d : %3d]\n' % (ResultadoRansac[1][match][0], ResultadoRansac[1][match][1], ResultadoRansac[2][match][0], ResultadoRansac[2][match][1]))
+        file.write('[%3d : %3d]  [%3d : %3d]\n' % (ResultadoRansac[1][match][1], ResultadoRansac[1][match][0], ResultadoRansac[2][match][1], ResultadoRansac[2][match][0]))
     file.write('soma do ransac: %d -----==END==-----\n\n\n' % number_of_ransac_confirmations)
     
     file.close()
 
     return
 
+
+
 # compare both match types
-def emain():
+def lolmain():
     MatchTeste = gen_graph.graph_routine(testeImg)
     MatchTeste2 = gen_graph.graph_routine(testeImg2)
     
@@ -113,7 +146,7 @@ def emain():
     
     i = 0
     while i < amout_of_repetition :
-        print('Next Batch')
+        print('Next Batch ', i)
         i += 1
         temp = 0
         
@@ -141,6 +174,26 @@ def emain():
 
     return
 
+
+
+# bug FINDER
+def mmain() :
+    ImageTeste = gen_graph.graph_routine(bug_test)
+    
+    file = open('results/testeNovoGrafico_Bug.txt', 'w')
+
+    for Vertex in range(len(ImageTeste.vertexes)) :
+        file.write('Vertice %d :\n' % Vertex)
+        file.write('    [%d : %d] :\n' % (ImageTeste.vertexes[Vertex].yx[1] , ImageTeste.vertexes[Vertex].yx[0]))
+        file.write('Vizinhos %d :\n' % len(ImageTeste.vertexes[Vertex].neighs))
+        for neigh in range(len(ImageTeste.vertexes[Vertex].neighs)) :
+            file.write('      i: %d ||' % ImageTeste.vertexes[Vertex].neighs[neigh].i)
+            file.write(' [%d : %d] :  ' % (ImageTeste.vertexes[ImageTeste.vertexes[Vertex].neighs[neigh].i].yx[1], ImageTeste.vertexes[ImageTeste.vertexes[Vertex].neighs[neigh].i].yx[0]))
+            file.write('ang: %f | dist: %f\n' % (ImageTeste.vertexes[Vertex].neighs[neigh].ang , ImageTeste.vertexes[Vertex].neighs[neigh].dist))
+        file.write('-----===-----\n\n')
+    file.close()
+    
+    return
 
 if __name__ == "__main__":
     main()
